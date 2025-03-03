93e4f09d8239984186165b5d110060a11782a935
EsVagy42 <sltamagotchi@gmail.com>
2025-03-03 18:21:14 +0000

    Added shortcuts

--- config/default.nix
+++ config/default.nix
@@ -7,4 +7,14 @@
     shiftwidth = 2;
   };
   globals = { mapleader = " "; };
+  extraConfigLua = ''
+    vim.keymap.set("n", "<leader>pb", ":Pick buffers<CR>", {desc = "Pick buffers"})
+    vim.keymap.set("n", "<leader>pd", ":Pick diagnostic<CR>", {desc = "Pick diagnostic"})
+    vim.keymap.set("n", "<leader>pe", ":Pick explorer<CR>", {desc = "Pick explorer"})
+    vim.keymap.set("n", "<leader>pf", ":Pick files<CR>", {desc = "Pick files"})
+    vim.keymap.set("n", "<leader>pg", ":Pick grep_live<CR>", {desc = "Pick grep live"})
+    vim.keymap.set("n", "<leader>ph", ":Pick git_hunks<CR>", {desc = "Pick git hunks"})
+    vim.keymap.set("n", "<leader>pl", ":Pick lsp<CR>", {desc = "Pick lsp"})
+    vim.keymap.set("n", "<leader>pm", ":Pick marks<CR>", {desc = "Pick marks"})
+  '';
 }
