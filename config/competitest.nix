66ee299f3109e221025198219b76ba4e77c6fa7e
EsVagy42 <sltamagotchi@gmail.com>
2025-03-16 23:49:35 +0000

    Changed config for competitest

--- config/competitest.nix
+++ config/competitest.nix
@@ -1,1 +1,27 @@
-{ plugins = { competitest = { enable = true; }; }; }
+{
+  plugins = {
+    competitest = {
+      enable = true;
+      settings = {
+        compile_command = {
+          cpp = {
+            exec = "g++";
+            args = [
+              "-Wall"
+              "$(FNAME)"
+              "-o"
+              "$(FNOEXT)"
+              "-g"
+              "-Wextra"
+              "-Wshadow"
+              "-Wconversion"
+              "-Wfloat-equal"
+              "-Wduplicated-cond"
+              "-Wlogical-op"
+            ];
+          };
+        };
+      };
+    };
+  };
+}
